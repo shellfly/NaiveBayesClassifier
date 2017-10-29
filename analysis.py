78dfdbb1495b100666685e4d7c981b884e70d150
shellfly <shell0fly@gmail.com>
2017-10-29 22:12:40 +0800

    update code

--- analysis.py
+++ analysis.py
@@ -6,11 +6,10 @@ import jieba
 
 jieba.add_word("村上春树")
 jieba.add_word("夏目漱石")
-jieba.add_word("细思极恐")
 stopwords = set([l.strip() for l in open('stopwords.txt')])
 
 topics = {
-    19551556:"旅行",
+    19551556: "旅行",
     19789286: "日语学习",
     19562563: "日剧",
     19860581: "JLPT",
@@ -19,14 +18,17 @@ topics = {
     19597091: "日本文学",
     19591985: "动漫"
 }
+
+
 def tokenize(content):
-    content = content.replace(' ', '').replace('\n', '');
+    content = content.replace(' ', '').replace('\n', '')
     return [w for w in jieba.cut(content) if len(w) > 1 and w not in stopwords]
+
 
 def build_dictionary(num):
     dictionary = set()
     for name in topics.values():
-        words = [w for l in open(name+".txt") for w in tokenize(l)]
+        words = [w for l in open(name + ".txt") for w in tokenize(l)]
         words = Counter(words)
         dictionary |= set(w[0] for w in words.most_common(num))
     return dictionary
@@ -34,27 +36,34 @@ def build_dictionary(num):
 dictionary = build_dictionary(100)
 print('dictionary length: ', len(dictionary))
 
+
 def gen_features(content):
     words = list(tokenize(content))
-    return {w: words.count(w) for w in dictionary}
+    return {w: w in words for w in dictionary}
 
 original_featuresets = []
 for name in topics.values():
-    content = open(name+".txt").read()
+    content = open(name + ".txt").read()
     items = [i for i in content.split('\n\n') if i]
-    original_featuresets.extend([(gen_features(item), name, item) for item in items])
+    original_featuresets.extend(
+        [(gen_features(item), name, item) for item in items])
 
 random.shuffle(original_featuresets)
 featuresets = [of[:2] for of in original_featuresets]
 print('all feature sets: ', len(featuresets))
-train_set, devtest_set, test_set = featuresets[200:], featuresets[100:200], featuresets[:100]
+train_set, devtest_set, test_set = featuresets[
+    200:], featuresets[100:200], featuresets[:100]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
-print(nltk.classify.accuracy(classifier, devtest_set))
+print('accuracy: ', nltk.classify.accuracy(classifier, devtest_set))
 
 
-errors = []
-for (features, name, item) in original_featuresets[100:200]:
-    guess = classifier.classify(features)
-    if guess != name:
-        print( 'name: ', name, 'guess: ', guess)
-        print([(f,count) for f,count in features.items() if count])
+for (features, name, item) in original_featuresets[10:20]:
+    print('item : ', item.replace('\n', '')[:30])
+    print('category: %s, guess: %s' % (name, classifier.classify(features)))
+
+# errors = []
+# for (features, name, item) in original_featuresets[100:200]:
+#     guess = classifier.classify(features)
+#     if guess != name:
+#         print('item: ', item, 'name: ', name, 'guess: ', guess)
+#         print([(f, a) for f, a in features.items() if a])
--- crawl.py
+++ crawl.py
@@ -1,3 +1,4 @@
+import os
 import random
 import time
 
@@ -6,10 +7,11 @@ import requests
 
 
 cookies = dict([a.split('=', 1) for a in """cookies from browser""".split(';')])
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 topics = {
-    19551556:"旅行",
+    19551556: "旅行",
     19789286: "日语学习",
     19562563: "日剧",
     19860581: "JLPT",
@@ -21,19 +23,27 @@ topics = {
 
 top_url = 'https://www.zhihu.com/topic/%s/top-answers?page=%s'
 
-def crawl_topic_top_answers(topic_id):
-    f = open(topics[topic_id]+'.txt', 'w')
-    for page in range(1,6):
+
+def crawl_topic_top_answers(topic_id, name):
+    filepath = name + '.txt'
+    if os.path.exists(filepath):
+        return
+
+    contents = []
+    for page in range(1, 11):
         url = top_url % (topic_id, page)
-        print('crawl url: ', url )
+        print('crawl url: ', url)
         res = requests.get(url, headers=headers, cookies=cookies)
         soup = BeautifulSoup(res.content, "lxml")
         for item in soup.find_all(class_="feed-item"):
             title = item.find("a", class_="question_link").text
             summary = item.find(class_="summary").text.replace("显示全部", "")
-            f.write("%s\n%s\n\n" % (title.strip(), summary.strip()))
+            contents.append("%s\n%s" % (title.strip(), summary.strip()))
 
-for topic in topics.keys():
-    crawl_topic_top_answers(topic)
+    with open(filepath, 'w') as f:
+        f.write('\n\n'.join(contents))
+
+for topic_id, name in topics.items():
+    crawl_topic_top_answers(topic_id, name)
     print('sleeping for next task...')
-    time.sleep(random.randint(0,10))
+    time.sleep(random.randint(30, 100))
